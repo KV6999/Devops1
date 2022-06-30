9cf59d90828b1cff52b38de062cf788fe0950b40
KV6999 <51716775+KV6999@users.noreply.github.com>
2022-06-30 19:06:22 +0530

    Update sum.sh

--- sum.sh
+++ sum.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 #this is an program to Find Sum
 echo "Ënter a Number"
-read Num
-
-sum=1
-
-while [ $Num -gt 1 ]
-do 
-	sum=`expr $sum \* $Num`
-	Num=`expr $Num - 1`
-done
-echo "Result is $sum"
+num=10
+num=50
